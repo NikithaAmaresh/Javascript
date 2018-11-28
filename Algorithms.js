2a5167a1d58b01a511666cf338173c85dd72bb82
NikithaAmaresh <nikitha.rai@gmail.com>
2018-11-28 11:41:27 +0530

    Update Algorithms.js

--- Algorithms.js
+++ Algorithms.js
@@ -1,1 +1,23 @@
+/** Fibonacci series without recurssion **/
 
+function fib(n){
+  var i;
+  var prev;
+  var curr;
+  var newVal;
+  var fibSeries;
+   for(i=0;i<n;i++){
+     if(i <= 1){
+       fibSeries = 1 + "\t";
+       prev = 1;
+       curr = 1;
+       return ;
+     }
+     newVal = prev + curr;
+     fibSeries =  newVal+ "\t";
+     prev = curr;
+     curr = newVal;
+    
+  }
+  return fibSeries;
+}
